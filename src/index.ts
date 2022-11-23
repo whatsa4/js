39f038af72d155032d78404077a057876b2e3bfb
Anthony Lekan <anthonylekan@Anthonys-Air-4.nj.rr.com>
2022-11-23 00:41:59 -0500

    domains.js - readme.md

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ export { Profile } from './profile';
 export { registerProfile, setProperties, removeProperties } from './profile/methods';
 export { getProfile } from './profile/queries';
 
-export { Domain, Resolver } from './domain';
+export { DomainNFT, Resolver } from './domain';
 export { registerDomain, extendRegistration, setRecords, deleteRecords } from './domain/methods';
-export { getDomain, getResolver } from './domain/queries';
+export { getResolver, getDomainNFT, getDomainOwner } from './domain/queries';
