708fb89b67ed7d22e292cc44f39327ea46a45071
Anthony <anthony@sns.domains>
2023-01-11 04:39:30 -0500

    domain - queries: added return null as option

--- src/domain/queries.ts
+++ src/domain/queries.ts
@@ -4,24 +4,28 @@ import getTableId from "../util/getTableId";
 
 async function getDomain(provider: JsonRpcProvider, name: string): Promise<Domain> {
     const resolver = await getResolver(provider, name);
-    const domainObjectResponse = await provider.getObject(resolver.domain);
-    const domainObject = domainObjectResponse.details as SuiObject;
+    if(resolver) {
+        const domainObjectResponse = await provider.getObject(resolver.domain);
+        const domainObject = domainObjectResponse.details as SuiObject;
 
-    const domainOwner = domainObject['AddressOwner'];
-    const domainFields = (domainObject.data as SuiMoveObject).fields;
+        const domainOwner = domainObject['AddressOwner'];
+        const domainFields = (domainObject.data as SuiMoveObject).fields;
 
-    return {
-        id: domainFields.id,
-        collection: domainFields.collection,
-        owner: domainOwner.toString(),
+        return {
+            id: domainFields.id,
+            collection: domainFields.collection,
+            owner: domainOwner.toString(),
 
-        name: domainFields.name,
-        tld: domainFields.tld,
-        attributes: domainFields.attributes,
-        url: domainFields.url,
+            name: domainFields.name,
+            tld: domainFields.tld,
+            attributes: domainFields.attributes,
+            url: domainFields.url,
 
-        expiration: domainFields.expiration,
-        timestamp: domainFields.timestamp,
+            expiration: domainFields.expiration,
+            timestamp: domainFields.timestamp,
+        }
+    } else {
+        return null;
     }
 }
 
@@ -32,15 +36,20 @@ async function getResolver(provider: JsonRpcProvider, name: string): Promise<Res
     const resolvers = tableFields.resolvers;
 
     const resolverId = resolvers[name.toLowerCase()];
-    const resolverObjectResponse = await provider.getObject(resolverId);
-    const resolverFields = ((resolverObjectResponse.details as SuiObject).data as SuiMoveObject).fields;
-    return {
-        id: resolverFields.id,
-        domain: resolverFields.domain,
-        records: resolverFields.records,
-        subdomains: resolverFields.subdomains,
-        expiration: resolverFields.expiration
-    };
+
+    if(resolverId) {
+        const resolverObjectResponse = await provider.getObject(resolverId);
+        const resolverFields = ((resolverObjectResponse.details as SuiObject).data as SuiMoveObject).fields;
+        return {
+            id: resolverFields.id,
+            domain: resolverFields.domain,
+            records: resolverFields.records,
+            subdomains: resolverFields.subdomains,
+            expiration: resolverFields.expiration
+        };
+    } else {
+        return null;
+    }
 }
 
 export { getDomain, getResolver };
